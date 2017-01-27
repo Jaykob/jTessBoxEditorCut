fcf42d06fbd3ed3b165f88fa6acd02fed908fe71
Jakob Raible <jakob@formigas.de>
2017-01-27 11:30:48 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jTessBoxEditorCut
-Fork of jTessBoxEditor adding a feature to save separate cropped images of recognized characters
+Fork of jTessBoxEditor adding a feature to save separate cropped images of recognized characters from training.
+In the Box Editor tab, clicking on Cut Images will save your characters in separate images. This can be useful for training neural networks, boosting, etc.
+
+Currently only confirmed to run in IntelliJ on macOS.
+
+You can invert the images by commenting out lines 1520-1524 in Gui.java.
---
+++ src/net/sourceforge/tessboxeditor/testForm.java
@@ -0,0 +1,40 @@
+/*
+ * Created by JFormDesigner on Wed Jan 21 15:13:44 CET 2015
+ */
+
+package net.sourceforge.tessboxeditor;
+
+import javax.swing.*;
+import com.jgoodies.forms.layout.*;
+
+/**
+ * @author unknown
+ */
+public class testForm extends JPanel {
+    public testForm() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+
+        // JFormDesigner evaluation mark
+        setBorder(new javax.swing.border.CompoundBorder(
+            new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+                "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+                javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+                java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+        setLayout(new FormLayout(
+            "default, $lcgap, default",
+            "2*(default, $lgap), default"));
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
